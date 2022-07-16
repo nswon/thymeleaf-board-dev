3c90b93255740a3de16eff31cf80d71207c798d0
nswon <swnam0729@gmail.com>
2022-07-16 21:30:31 +0900

    FEAT :: (#11) 프로필 페이지

--- src/main/java/study/thymeleafstudy/domain/member/domain/repository/MemberRepository.java
+++ src/main/java/study/thymeleafstudy/domain/member/domain/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package study.thymeleafstudy.domain.member.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.thymeleafstudy.domain.member.domain.Member;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
 }
--- src/main/java/study/thymeleafstudy/domain/member/service/MemberService.java
+++ src/main/java/study/thymeleafstudy/domain/member/service/MemberService.java
@@ -1,8 +1,10 @@
 package study.thymeleafstudy.domain.member.service;
 
 import study.thymeleafstudy.domain.member.presentation.dto.request.MemberRequestDto;
+import study.thymeleafstudy.domain.member.presentation.dto.response.MemberResponseDto;
 
 public interface MemberService {
 
     Long join(MemberRequestDto requestDto);
+    MemberResponseDto findMember(Long id);
 }
